ac5cae55c3ebf6b91d6bf3639453f7e62afaf66d
Akhil Dhyani <akhildhyani420@gmail.com>
2024-10-18 01:18:44 +0530

    gui initial try
    .

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
-int main() {
-    int a;
-    int b;
-    
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window;
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_title(GTK_WINDOW(window), "Maze Solver");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
 }
